38bca9b24b322052504e2379c14db3590ca56fc9
Ender Doe <ender@shpazzle.com>
2024-10-18 09:25:00 -0700

    More changes to bug

--- WIP_Scenes/Leg.gd
+++ WIP_Scenes/Leg.gd
@@ -2,33 +2,94 @@ extends Polygon2D
 
 class_name SegmentLeg
 
-@export var range_rad = PI / 4
+var EPS = 0.01
 
+# Arm lengths
+@export var l1 = 100.0  # Length of the first segment (upper arm)
+@export var l2 = 80.0   # Length of the second segment (forearm)
+
+# Joint constraints in radians
+@export var shoulder_min = deg_to_rad(-30.0) # fowrard angle limit
+@export var shoulder_max = deg_to_rad(60.0) # backward angle limit
+
+@export var elbow_min = deg_to_rad(-20.0)       # 0 degrees (straight arm)
+@export var elbow_max = deg_to_rad(20.0)     # 135 degrees bent
+
+@export var shoulder_rotate_speed = 2
+@export var elbow_rotate_speed = 3
+# Target to reach (set this to a position in your scene)
+var target_pos = null
+var is_grabbing: bool = false
+var grabbed_pos = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _draw() ->void:
+	draw_circle(to_local(target_pos), 6, Color(1, 0, 1))
+	if grabbed_pos:
+		draw_circle(to_local(grabbed_pos), 6, Color(1, 1, 0))
 	pass
 
+func _process(delta):
+	if target_pos == null:
+		return		
+	queue_redraw()
+	# If we are grabbed make that the target	
+	var target = grabbed_pos if grabbed_pos != null else target_pos
+		
+	# Update the IK every frame
+	var ik_result = solve_ik(Vector2(0,0), to_local(target), l1, l2)
+	if ik_result:
+		var angle1 = ik_result[0]
+		var angle2 = ik_result[1]
+		var actual_target = to_global(ik_result[2])
+		
+		# If we are grabbing then we assume external forces are rotating our joints freely		
+		var grab_speed_modifier = 10000 if is_grabbing else 1
+		
+		$Femur.rotation = rotate_toward($Femur.rotation, angle1, delta * shoulder_rotate_speed * grab_speed_modifier)
+		$Femur/Tibia.rotation = rotate_toward($Femur/Tibia.rotation, angle2, delta * elbow_rotate_speed * grab_speed_modifier)
+		
+		# Check how far off we are from grabbed pos or actual target		
+		var p = grabbed_pos if grabbed_pos != null else actual_target
+		
+		if p.distance_to($Femur/Tibia/Hand.global_position) < EPS:		
+			is_grabbing = true
+			grabbed_pos = actual_target
+		else:
+			is_grabbing = false
+			grabbed_pos = null
+			
+		$Femur/Tibia/Hand.color = Color(1, 0, 0) if is_grabbing else Color(0, 0, 1)
+
+# Inverse Kinematics Solver (2D two-segment arm)
+func solve_ik(base_pos: Vector2, target: Vector2, length1: float, length2: float) -> Array:
+	# Get the distance between the base and the target
+	var distance = base_pos.distance_to(target)
 	
-func get_capacity(target: Vector2):
-	var direction_to_target = (target - position).normalized()	
-	var target_angle = direction_to_target.angle()
-	var ideal_leg_push_angle = (- direction_to_target).angle()
-	var ideal_leg_push_angle_local = ideal_leg_push_angle - global_rotation
-	ideal_leg_push_angle_local = wrapf(ideal_leg_push_angle_local, -range_rad, range_rad)
-	var actual_angle = ideal_leg_push_angle_local + global_rotation
-	var vec2 = Vector2(cos(actual_angle), sin(actual_angle))
-	return {
-		"vec2": vec2,
-		"max_force": 1
-	}
+	# Clamp the distance to within reachable range
+	distance = clamp(distance, abs(length1 - length2), length1 + length2)
 	
+	# Law of cosines to calculate the angle at the elbow
+	var cos_angle2 = (pow(length1, 2) + pow(length2, 2) - pow(distance, 2)) / (2 * length1 * length2)
+	var angle2 = acos(-cos_angle2)  # Angle at the elbow (θ2)
 	
+	# Angle between base to target and the first segment
+	var angle1_to_target = atan2(target.y - base_pos.y, target.x - base_pos.x)
 	
+	# Law of cosines to calculate the shoulder angle (θ1)
+	var angle1_part = acos((pow(length1, 2) + pow(distance, 2) - pow(length2, 2)) / (2 * length1 * distance))
+	var angle1 = angle1_to_target - angle1_part
 	
+	# Clamp the angles to respect joint constraints
+	angle1 = clamp(angle1, shoulder_min, shoulder_max)
+	angle2 = clamp(angle2, elbow_min, elbow_max)
+	
+	# The actual target with the constraints applied	
+	var actual_target = base_pos + Vector2.RIGHT.rotated(angle1) * l1 +  Vector2.RIGHT.rotated(angle1 + angle2) * l2
+	
+	# Return both angles
+	return [angle1, angle2, actual_target]
--- WIP_Scenes/segment.gd
+++ WIP_Scenes/segment.gd
@@ -1,61 +1,97 @@
-extends Polygon2D
+extends RigidBody2D
 
-class_name Segment
-
-enum SegmentType {
-	Head,
-	Body
-}
-
-@export var front_socket_position: Vector2 = Vector2(35, 0)
-@export var back_socket_position: Vector2 = Vector2(-35, 0)
-@export var front_segment: Segment;
-@export var back_segment: Segment;
-@export var segment_type: SegmentType = SegmentType.Body;
+@export var target: Node2D;
+@export var max_speed = 200
+@export var max_angular_velocity = 200
 
 @export var legs : Array[SegmentLeg] = [];
 
-@export var turn_speed_rad: float = PI / 32;
-@export var speed_px: float = 20;
-@export var target: Node2D
+@export var leg_grab_abandon_angle = deg_to_rad(80)
+	
+	
+func draw_vector_head(start: Vector2, end: Vector2, arrow_size=10, arrow_width =5):
+	var direction = (end - start).normalized()
+	var perpendicular = Vector2(-direction.y, direction.x)
 
-enum SegmentMessage {
-	Breathing=0
-}
+	# Calculate the two points for the arrowhead lines
+	var arrow_point1 = end - (direction * arrow_size) + (perpendicular * arrow_width)
+	var arrow_point2 = end - (direction * arrow_size) - (perpendicular * arrow_width)
 
+	# Draw the arrowhead
+	draw_line(end, arrow_point1, Color(1, 0, 0), 2)  # Left side of arrow
+	draw_line(end, arrow_point2, Color(1, 0, 0), 2)  # Right side of arrow
 
-func recieve_message(change_in_position: Vector2, change_in_rotation: float) -> void:
-	position += change_in_position
+func draw_vector(start: Vector2, end: Vector2, color: Color = Color(1, 0, 0), width: float = 6, arrow_size: float =10, arrow_width: float =5):
+	# Draw the vector (main line)
+	draw_line(start, end, color, width)  # Red vector line
+		
+	# Draw the vector head (arrow)
+	draw_vector_head(start, end, arrow_size, arrow_width)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+var vecs_to_draw = []
+func _draw():
+	draw_vector(Vector2(0,0), Vector2(600, 0), Color(1, 1, 0), 20)
+	var leg_groups = [
+		[legs[0], legs[1]],
+		[legs[2], legs[3]],
+		[legs[4], legs[5]]
+	]
+	for leg_g in leg_groups:
+		if leg_g[0].target_pos and leg_g[1].target_pos:
+			draw_line(to_local(leg_g[0].target_pos), to_local(leg_g[1].target_pos), Color(1,1,1), 10, true)
+			
+	for vec in vecs_to_draw:
+		draw_vector(vec[0], vec[1], vec[2], vec[3])
+	vecs_to_draw = []
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if segment_type == SegmentType.Head and target:
-		
+func _physics_process(delta: float) -> void:
+	queue_redraw()
+	var direction_to_target = (target.global_position - global_position).normalized()	
+	
+	for leg in legs:
+		if leg.is_grabbing:
+			var dir_F = (legs[0].global_position - legs[0].get_node("./Femur/Tibia").global_position).normalized()
+			dir_F = Vector2(-dir_F.y, dir_F.x)
+			if dir_F.angle_to(direction_to_target) > leg_grab_abandon_angle:
+				leg.is_grabbing = false
+			
+	var leg_groups = [
+		[legs[0], legs[1]],
+		[legs[2], legs[3]],
+		[legs[4], legs[5]]
+	]
+	
+	for leg_g in leg_groups:
+		var leg1 = leg_g[0]
+		var leg2 = leg_g[1]
+		var segment_c =  (leg1.global_position + leg2.global_position)/2
+		var target_to_leg = (target.global_position  - segment_c).normalized()
+		var cros = Vector2(-target_to_leg.y, target_to_leg.x)
+		leg1.target_pos = leg1.global_position + cros * 130  * -1
+		leg2.target_pos = leg2.global_position + cros * 130 * 1	
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		for leg in legs:
-			var c = leg.get_capacity(target.global_position)
-			c[""]
-		
-		#var direction_to_target = (target.position - position).normalized()
-		#
-		#var target_angle = direction_to_target.angle()
-		#
-		#var angle_difference = target_angle - rotation
-		#
-		#angle_difference = wrapf(angle_difference, -PI, PI)
-			#
-		#var new_rotation = rotate_toward(rotation, angle_difference,turn_speed_rad * delta)
-		#var new_position = position + delta * direction_to_target * speed_px
-		#
-		#var change_in_position = new_position - position
-		#var change_in_rotation = new_rotation - rotation
-		#
-		#rotation = new_rotation
-		#position = new_position
-		#
-		#if back_segment:
-			#back_segment.recieve_message(change_in_position, change_in_rotation)
+			if !leg.is_grabbing: continue
+			
+			# 
+			var dir_F = (legs[0].global_position - legs[0].get_node("./Femur/Tibia").global_position).normalized()
+			dir_F = Vector2(-dir_F.y, dir_F.x)
+			
+			# If the force is not towards the target don't activate the muscle			
+			if dir_F.angle_to(direction_to_target) > leg_grab_abandon_angle:
+				continue	
+				
+			var F = dir_F * 4
+			
+			vecs_to_draw.push_back([to_local(leg.global_position), to_local(leg.global_position) + F * 40, Color(1, 0, 0), 10])
+			
+			apply_force(F * 4, legs[0].global_position)
+			
+	
