c483ca43963ffffd6f2aa9c4e40474c22b804ad4
Kevin Schlereth III <kev2.tablet@gmail.com>
2024-10-21 17:22:00 -0400

    Updated Day Night Cycle
    Slight modifications

--- Scripts/daylight_cycle.gd
+++ Scripts/daylight_cycle.gd
@@ -1,6 +1,7 @@
 extends Node
 
-@export var day_length_in_seconds: float = 300.0 
+@export var day_length_in_seconds: float = 600.0 #10 minutes
+@export var default_start_time: float = 0.2
 var current_time_of_day: float = 0.0  
 
 @export var time_mult: float = 1.0
@@ -11,12 +12,16 @@ var sunrise_time: float = 0.2
 var noon_time: float = 0.5     
 var sunset_time: float = 0.8  
 
+var daylight_state: SignalBus.DaylightState = SignalBus.DaylightState.NIGHT
+
 func _ready():
+	current_time_of_day = default_start_time
 	set_process(true)
 
 func _process(delta):
 	update_time_of_day(delta)
 	adjust_lighting()
+	check_daylight_state()
 
 func update_time_of_day(delta):
 	current_time_of_day += (delta / day_length_in_seconds) * time_mult
@@ -32,6 +37,27 @@ func adjust_lighting():
 		light_node.energy = lerp(1.5, 1.0, (current_time_of_day - noon_time) / (sunset_time - noon_time))
 	else:
 		light_node.energy = lerp(1.0, 0.2, (current_time_of_day - sunset_time) / (1.0 - sunset_time))
+
+func check_daylight_state():
+	if current_time_of_day <= sunrise_time:
+		if daylight_state != SignalBus.DaylightState.MORNING:
+			daylight_state = SignalBus.DaylightState.MORNING
+			emit_day_state_changed(daylight_state)
+	elif current_time_of_day <= noon_time:
+		if daylight_state != SignalBus.DaylightState.DAY:
+			daylight_state = SignalBus.DaylightState.DAY
+			emit_day_state_changed(daylight_state)
+	elif current_time_of_day <= sunset_time:
+		if daylight_state != SignalBus.DaylightState.EVENING:
+			daylight_state = SignalBus.DaylightState.EVENING
+			emit_day_state_changed(daylight_state)
+	else:
+		if daylight_state != SignalBus.DaylightState.NIGHT:
+			daylight_state = SignalBus.DaylightState.NIGHT
+			emit_day_state_changed(daylight_state)
+
+func emit_day_state_changed(new_state: SignalBus.DaylightState):
+	SignalBus.switch_day_state.emit(new_state)
 
 func set_time_of_day(time: float):
 	current_time_of_day = clamp(time, 0.0, 1.0)
