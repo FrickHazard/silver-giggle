1b09a70db663c04e336ddd136bcf7b86afd8e5af
Kevin Schlereth III <kev2.tablet@gmail.com>
2024-10-22 17:02:54 -0400

    Added Day/Night Door
    A door that opens and closes on specific triggers. Maybe part of the map is only accessible at night...

---
+++ Scripts/daylight_door.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@export var open_trigger: SignalBus.DaylightState
+@export var close_trigger: SignalBus.DaylightState
+
+@export var door_collider: CollisionShape2D
+@export var door_visual: Sprite2D
+
+func _ready():
+	SignalBus.switch_day_state.connect(_on_day_state_changed)
+
+func _on_day_state_changed(new_state):
+	if new_state == open_trigger:
+		open_door()
+	elif new_state == close_trigger:
+		close_door()
+	pass
+	
+func open_door():
+	door_collider.disabled = true
+	door_visual.visible = false
+	#play noise
+	#play animation
+	pass
+	
+func close_door():
+	door_collider.disabled = false
+	door_visual.visible = true
+	#play noise
+	#play animation
+	pass
